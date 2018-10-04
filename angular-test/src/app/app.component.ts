9e1b96bb3821e09b170dbf8ece7455d1b415c7db
ANDRE Jérôme <andre.jerome@protonmail.com>
2018-10-04 22:37:27 +0200

    Angular Middle project

--- angular-test/src/app/app.component.ts
+++ angular-test/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppareilService} from "./services/appareil.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+  }
+
 }
