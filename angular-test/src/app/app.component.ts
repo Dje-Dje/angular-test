b78b648136fb797f77d36f7e37590fbb055db243
ANDRE Jérôme <andre.jerome@protonmail.com>
2018-10-05 00:34:03 +0200

    Angular Middle project

--- angular-test/src/app/app.component.ts
+++ angular-test/src/app/app.component.ts
@@ -1,18 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {AppareilService} from "./services/appareil.service";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
-  constructor(){
+  secondes : number;
+  counterSubscription : Subscription;
+
+  constructor(){}
+
+  ngOnInit() {
+
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      }
+    );
 
   }
 
-  ngOnInit(): void {
+  ngOnDestroy(){
+    this.counterSubscription.unsubscribe();
   }
-
 }
--- angular-test/src/app/appareil-view/appareil-view.component.ts
+++ angular-test/src/app/appareil-view/appareil-view.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AppareilService} from "../services/appareil.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-appareil-view',
@@ -22,6 +23,7 @@ export class AppareilViewComponent implements OnInit {
   );
 
   appareils: any[];
+  appareilsSubscription : Subscription;
 
   constructor(private appareilService: AppareilService) {
     setTimeout(
@@ -32,7 +34,12 @@ export class AppareilViewComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.appareils = this.appareilService.appareils;
+    this.appareilsSubscription = this.appareilService.appareilSubject.subscribe(
+      (appareils: any[]) => {
+        this.appareils = appareils;
+      }
+    );
+    this.appareilService.emitAppareilSubject();
   }
 
   onAllumer() {
