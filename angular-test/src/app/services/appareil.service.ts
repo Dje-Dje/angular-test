7432e594ff8e8d37446436aa8bd1f4776fde77be
ANDRE Jérôme <andre.jerome@protonmail.com>
2018-10-07 13:47:25 +0200

    Bookshelves project initialization

--- angular-test/src/app/services/appareil.service.ts
+++ angular-test/src/app/services/appareil.service.ts
@@ -1,33 +1,24 @@
 import {Subject} from "rxjs";
 import {st} from "@angular/core/src/render3";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+
+@Injectable()
 
 export class AppareilService {
 
   appareilSubject = new Subject<any[]>();
 
- private appareils = [
-    {
-      id: 1,
-      name: "Machine à laver",
-      status: "éteint"
-    },
-    {
-      id: 2,
-      name: "Télévision",
-      status: "allumé"
-    },
-    {
-      id: 3,
-      name: "Ordinateur",
-      status: "éteint"
-    }
-  ];
+  private appareils = [];
 
- emitAppareilSubject(){
-   this.appareilSubject.next(this.appareils.slice());
- }
+  constructor(private httpClient: HttpClient) {
+  }
 
-  getAppareilById(id: number){
+  emitAppareilSubject() {
+    this.appareilSubject.next(this.appareils.slice());
+  }
+
+  getAppareilById(id: number) {
     const appareil = this.appareils.find(
       (appareilObject) => {
         return appareilObject.id === id;
@@ -50,28 +41,55 @@ export class AppareilService {
     this.emitAppareilSubject();
   }
 
-  switchOnOne(index : number) {
+  switchOnOne(index: number) {
     this.appareils[index].status = "allumé";
     this.emitAppareilSubject();
   }
 
-  switchOffOne(index : number) {
+  switchOffOne(index: number) {
     this.appareils[index].status = "éteint";
     this.emitAppareilSubject();
   }
 
-  addAppareil(name : string, status: string){
-   const appareilObject = {
-     id: 0,
-     name,
-     status
-   };
-   appareilObject.name = name;
-   appareilObject.status = status;
-   appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
+  addAppareil(name: string, status: string) {
+    const appareilObject = {
+      id: 0,
+      name,
+      status
+    };
+    appareilObject.name = name;
+    appareilObject.status = status;
+    appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
 
-   this.appareils.push(appareilObject);
-   this.emitAppareilSubject();
+    this.appareils.push(appareilObject);
+    this.emitAppareilSubject();
+  }
+
+  saveAppareilsToServer() {
+    this.httpClient
+      .put('https://http-client-angular-test.firebaseio.com/appareils.json', this.appareils)
+      .subscribe(
+        () => {
+          console.log("Enregistrement terminé !");
+        },
+        (error) => {
+          console.log("Erreur de sauvegarde ! " + error);
+        }
+      )
+  }
+
+  getAppareilsFromServer(){
+    this.httpClient
+      .get<any>("https://http-client-angular-test.firebaseio.com/appareils.json")
+      .subscribe(
+        (response) => {
+          this.appareils = response;
+          this.emitAppareilSubject();
+        },
+        (error) => {
+          console.log("Erreur de chargement ! " + error)
+        }
+      );
   }
 
 }
